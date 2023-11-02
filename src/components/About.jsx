86c2858dacdd68d7514c6d0854be07c15872bfdd
@johnya0 <anoopjohny2000@gmail.com>
2023-11-02 11:26:08 +0100

    adding information to about.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,73 @@
-import React from 'react'
+import React from "react";
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
 
-export default About
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+        Highly motivated Bioinformatics Bachelor's graduate with first-class honors from the prestigious Saurashtra University, currently enrolled in the 5th semester of a Life Science Informatics Masters program. 
+        I'd like to use my extensive skill set to help you overcome your upcoming challenges. 
+        I previously worked as a student assistant from my bachelor's HOD in pursuit of goals that unquestionably required extensive use of software as required for this position. 
+        I hope I would be considered for this task. 
+        Among several of my achievements include, successfully obtaining 2 ECTs after completion of Elements of AI course conducted by the University of Helsinki. 
+        Python programming language (Part of curriculum and Add-on course)
+        Java Programming language (Part of curriculum)
+        C programming language (Part of curriculum)
+        Algorithms in Bioinformatics (Part of curriculum)
+        Machine learning and Data science (Add-on course)
+        PHP, JavaScript, and MySQL (Part of curriculum)
+        R language (Part of curriculum and Add-on course)
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
