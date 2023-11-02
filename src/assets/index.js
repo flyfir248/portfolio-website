625a31c066377e2239d0fab85f26ce85be2de8df
@johnya0 <anoopjohny2000@gmail.com>
2023-11-02 10:50:45 +0100

    commit: uploading files

---
+++ src/assets/index.js
@@ -0,0 +1,122 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import threejs from "./tech/threejs.svg";
+import youtube from "./tech/youtube.png";
+import bootstrap from "./tech/bootstrap.png";
+import blender from "./tech/blender.png";
+import c from "./tech/c.png";
+import flask from "./tech/flask.png";
+import java from "./tech/java.png";
+import linux from "./tech/linux.png";
+import mysql from "./tech/mysql.png";
+import photoshop from "./tech/photoshop.png";
+import php from "./tech/php.png";
+import python from "./tech/python.png";
+import unity from "./tech/unity.png";
+import tensorflow from "./tech/tensorflow.png";
+import pytorch from "./tech/pytorch.png";
+import selenium from "./tech/selenium.png";
+import opencv from "./tech/opencv.png";
+import pandas from "./tech/pandas.png";
+import unreal from "./tech/unreal.png";
+import springboot from "./tech/springboot.png";
+import godot from "./tech/godot.png";
+import plotly from "./tech/plotly.png";
+import linkedin from "./tech/linkedin.png";
+import stackoverflow from "./tech/stackoverflow.png";
+import kaggle from "./tech/kaggle.png";
+import typescript from "./tech/typescript.png";
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+import QACS from "./company/QACS.png";
+import BonnUni from "./company/BonnUni.jpg";
+import BBPS from "./company/BBPS.jpg";
+import SCAI from "./company/SCAI.png";
+import Christ_college from "./company/Christ_college.png";
+import TCS from "./company/TCS.jpg"
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  carrent,
+  jobit,
+  tripguide,
+  typescript,
+  TCS,
+  youtube,
+  bootstrap,
+  blender,
+  c,
+  flask,
+  java,
+  linux,
+  mysql,
+  photoshop,
+  php,
+  python,
+  unity,
+  tensorflow,
+  pytorch,
+  selenium,
+  opencv,
+  pandas,
+  unreal,
+  springboot,
+  godot,
+  plotly,
+  linkedin,
+  stackoverflow,
+  kaggle,
+  QACS,
+  BonnUni,
+  BBPS,
+  SCAI,
+  Christ_college
+};
